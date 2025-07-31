51abcf613eb29432c4101a27cb02491e65dddcef
Oladotun Kolapo <oladotunkolapo@gmail.com>
2025-07-31 13:33:26 +0100

    project architecture setup and package installation

--- backend/core/account/apps.py
+++ backend/core/account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BalancedPlateConfig(AppConfig):
+class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Balanced_Plate'
+    name = 'core.account'
