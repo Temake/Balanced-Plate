29cd383fbfa24801b751528b9deaab3c5c4c4476
Oladotun Kolapo <oladotunkolapo@gmail.com>
2025-08-26 16:16:52 +0100

    used flask for telegram bot

---
+++ bot/flask_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, abort
+import asyncio
+from telegram import Update
+from .core.main import telegram_app, TOKEN
+
+
+# Initialize the app manually
+asyncio.get_event_loop().run_until_complete(telegram_app.initialize())
+asyncio.get_event_loop().run_until_complete(telegram_app.start())
+
+app = Flask(__name__)
+
+@app.route(f"/{telegram_app.bot.token}", methods=["POST"])
+def webhook():
+    if not request.headers.get("content-type", "").startswith("application/json"):
+        abort(400)
+
+    data = request.get_json(force=True)
+    update = Update.de_json(data, telegram_app.bot)
+
+
+    asyncio.get_event_loop().create_task(telegram_app.process_update(update))
+    return "OK"
