e3a31bd0e3be0c2b4501bc3950bab1bbde99b702
Oladotun Kolapo <oladotunkolapo@gmail.com>
2026-01-30 22:06:10 +0100

    bugfix: debug toolbar error in production fix

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -4,15 +4,12 @@ from .base import *
 DATABASES = {
     "default": {
         "ENGINE": env.str(
-            "DJANGO_POSTGRESQL_ENGINE", "django.db.backends.postgresql_psycopg2"
+            "POSTGRESQL_ENGINE", "django.db.backends.postgresql_psycopg2"
         ),
-        "NAME": env.str("DJANGO_POSTGRES_NAME", "***"),
-        "USER": env.str("DJANGO_POSTGRES_USER", "***"),
-        "PASSWORD": env.str("DJANGO_POSTGRES_PASSWORD", "***"),
-        "HOST": env.str("DJANGO_POSTGRES_HOST", "*****"),
-        "PORT": env.int("DJANGO_POSTGRES_PORT", 5432),
+        "NAME": env.str("POSTGRES_NAME", "***"),
+        "USER": env.str("POSTGRES_USER", "***"),
+        "PASSWORD": env.str("POSTGRES_PASSWORD", "***"),
+        "HOST": env.str("POSTGRES_HOST", "*****"),
+        "PORT": env.int("POSTGRES_PORT", 5432),
     },
 }
-
-
-# STATIC_URL = "/static/"
