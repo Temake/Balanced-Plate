893902960d0aab616b1d241cbdc6befcd397d982
Oladotun Kolapo <oladotunkolapo@gmail.com>
2026-05-06 22:37:52 +0100

    background email messaging task

--- backend/core/utils/tasks/mail/__init__.py
+++ backend/core/utils/tasks/mail/__init__.py
@@ -1,18 +1,12 @@
 from celery import shared_task
 from loguru import logger
 
-from django.core.mail import send_mail
+from core.utils.helpers.email import EmailClient
 
 
 @shared_task
-def send_mail_async(subject, message, sender, recipients, html=None):
-    send_mail(
-        subject, 
-        message, 
-        sender, 
-        recipients, 
-        html_message=html, 
-        fail_silently=False
+def send_email_to_address(email_address, subject, message, name=""):
+    email_messaging_helper = EmailClient(
+        email_address, subject=subject, message=message, receiver_name=name
     )
-    logger.info(f"Email sent to {recipients} with subject: {subject}")
-    return True
+    email_messaging_helper.send_mail()
